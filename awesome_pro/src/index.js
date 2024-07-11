3049428a31482a9f50690a20f5c2b5f6a9bbcc6d
Krisha <you@example.com>
2024-07-11 17:47:32 +0530

    first commit

---
+++ awesome_pro/src/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+import Card from './Cards';
+import './index.css';
+
+ReactDOM.render(
+  <>
+  <svg viewBox="0 0 1320 300">
+	<text x="50%" y="50%" dy=".35em" text-anchor="middle">
+		telegram 2.0
+	</text>
+</svg>	
+<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+
+
+  <Card imgsrc="https://upload.wikimedia.org/wikipedia/en/thumb/d/da/DarkNetflixPosterEnglish.jpg/250px-DarkNetflixPosterEnglish.jpg"
+  title=" A Netflix Original Series"
+  sname="DARK"
+  link="https://www.netflix.com/in/title/80100172"
+  />
+ 
+  <Card imgsrc="https://upload.wikimedia.org/wikipedia/en/7/78/Stranger_Things_season_4.jpg"
+  title=" A Netflix Original Series"
+  sname="STANGER THINGS"
+  link="https://www.netflix.com/in/title/80057281"
+  />
+
+<Card imgsrc="https://static.toiimg.com/photo/msid-79298252/79298252.jpg?92708"
+  title=" A Netflix Original Series"
+  sname="MISMATCHED"
+  link="https://www.netflix.com/in/title/80220079"
+  />
+
+<Card imgsrc="https://static.toiimg.com/photo/msid-79298252/79298252.jpg?92708"
+  title=" A Netflix Original Series"
+  sname="MISMATCHED"
+  link="https://www.netflix.com/in/title/80220079"
+  />
+
+<Card imgsrc="https://upload.wikimedia.org/wikipedia/en/6/66/Wednesday_Netflix_series_poster.png"
+  title=" A Netflix Original Series"
+  sname="WEDNESDAY"
+  link="https://www.netflix.com/in/title/81231974"
+  />
+  </>
+  
+  ,document.getElementById("root")
+);
